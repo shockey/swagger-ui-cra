b0c5f02486f431b90529fb63f4904f932131f315
Kyle Shockey <shockey@users.noreply.github.com>
2020-06-23 19:33:12 -0500

    replace swagger-ui with swagger-ui-react

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react';
-import SwaggerUI from 'swagger-ui'
+import SwaggerUI from 'swagger-ui-react'
 import CustomLayout from './CustomLayout'
 import logo from './logo.svg';
 import './App.css';
-import 'swagger-ui/dist/swagger-ui.css';
+import 'swagger-ui-react/swagger-ui.css';
 
 const DOM_ID = "swagger-ui-mountpoint"
 
-const CustomLayoutPlugin = {
-  // package our CustomLayout component into a plugin,
-  // so Swagger-UI can use it
-  components: {
-    CustomLayout: CustomLayout
-  }
-}
-
 class App extends Component {
-  componentDidMount() {
-    SwaggerUI({
-      dom_id: `#${DOM_ID}`,
-      url: "http://petstore.swagger.io/v2/swagger.json",
-      plugins: [
-        CustomLayoutPlugin
-      ],
-      layout: "CustomLayout"
-    })
-  }
   render() {
     return (
-      <div className="App">
-        <div id={DOM_ID} />
-      </div>
+      <SwaggerUI url={"http://petstore.swagger.io/v2/swagger.json"}></SwaggerUI>
     );
   }
 }
